d98704bf1b7e0f07424ba66a57d3aff645572535
amalkhairin <amallanch45@gmail.com>
2019-03-22 21:48:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text, Button, TouchableOpacity, StatusBar} from 'react-native';
+
+class App extends Component {
+    render(){
+        return (
+            <View>
+	       		<Text>
+	       			cobaaaaa
+	       		</Text>     		
+	      	</View>
+        )
+    }
+}
+
+export default App;
